3836bc428039e6bd4746d549f9b8e799a8aedd1e
Mu qi <muqizhang640@gmail.com>
2022-01-01 17:37:41 +0100

    Initial Commit

---
+++ School Network/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  School Network
+//
+//  Created by Mu qi Zhang on 1/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ School Network/School_NetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  School_NetworkApp.swift
+//  School Network
+//
+//  Created by Mu qi Zhang on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct School_NetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
